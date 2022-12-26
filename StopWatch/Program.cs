c4870f2a72a57b233e0ae4a9e201d7dcdd1705f1
musta <musta@DESKTOP-44RR6PQ>
2022-12-26 12:35:43 +0200

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            StopWatch stopWatch = new StopWatch();
+
+            //for (int i = 0; i < 4; i++) // run the stop watch 4 times.
+            //{
+            //    stopWatch.Start();
+            //    Thread.Sleep(1000);
+            //    stopWatch.Stop();
+            //    Console.WriteLine(stopWatch.GetTime());
+            //    Console.WriteLine("Enter to run agein.");
+            //    Console.ReadKey();
+            //}
+            
+            //Run the stop watch only one time.
+            stopWatch.Start();
+            Thread.Sleep(1000);
+         //   stopWatch.Start();
+
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.GetTime());
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    internal class StopWatch
+    {
+        private DateTime StartTime { get; set; }
+        private DateTime EndTime { get; set; }
+        
+        private bool _isRunning; // inintialized to false
+
+        public void Start()
+        {
+            if (_isRunning == true) //the same as if (_isRunning) // if _isRunning = true tell the user the program is runnging
+            // to not call the start method twice in the program 
+            {
+                throw new Exception("The stop Watch is running now.\n");
+            }
+
+            this.StartTime = DateTime.Now;
+            _isRunning = true;
+
+
+
+        }
+        public void Stop()
+        {
+            if (_isRunning == false) // the same as if(!_isRunning) -- // if _iSrunning = false tell the user that the programs isn't even running
+            {
+                throw new Exception("The stop Watch isn't even running.\n");
+
+            }
+            EndTime = DateTime.Now;
+            _isRunning = false;
+        }
+
+        public TimeSpan GetTime()
+        {
+            return EndTime - StartTime;  
+        }
+    }
+}
